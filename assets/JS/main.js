a672b8bd687b8ea36257c2fd08a8254b7f6f0986
TheNoodleMoose <cjh605@gmail.com>
2018-11-28 11:18:23 -0500

    Added comments to files

--- assets/JS/main.js
+++ assets/JS/main.js
@@ -1,6 +1,8 @@
+// The initializes longitude and latitude as global variables
 let longitude;
 let latitude;
 
+// This is to initialize firebase
 var config = {
     apiKey: "AIzaSyAmqTyfDNVyzz6VG-WlM51ffi7jpB4hqH4",
     authDomain: "jd-c-project.firebaseapp.com",
@@ -10,63 +12,35 @@ var config = {
     messagingSenderId: "761811179231"
 };
 firebase.initializeApp(config);
+
+// This var holds the firebase database 
 const database = firebase.database();
 
-const txtEmail = document.getElementById('txtEmail');
-const txtPassword = document.getElementById('txtPassword');
-const btnLogin = document.getElementById('btnLogin');
-const btnSignUp = document.getElementById('btnSignUp');
-const btnLogout = document.getElementById('btnLogout');
-
-$("#btnLogin").on("click", function (event) {
-    event.preventDefault();
-    //Get Email and pass
-    const email = txtEmail.value;
-    const pass = txtPassword.value;
-    const auth = firebase.auth();
-    //Sign In
-    const promise = auth.signInWithEmailAndPassword(
-        email, pass);
-    promise.catch(e => console.log(e.message));
-});
-
-$("#btnSignUp").on("click", function (event) {
-    event.preventDefault();
-    //CHECK FOR REAL EMAIL
-    const email = txtEmail.value;
-    const pass = txtPassword.value;
-    const auth = firebase.auth();
-    //Sign In
-    const promise = auth.createUserWithEmailAndPassword(
-        email, pass);
-    promise.then(function () {
-        firebase.database().ref('users/' + uid).push({
-            username: name,
-            email: email,
-        });
-    });
-});
-
+// This logs the user out when the click the logout button
 $("#btnLogout").on("click", function (event) {
     event.preventDefault();
     firebase.auth().signOut();
     document.location.href = 'index.html';
 })
 
+// When a user signs in, if they are actually a user than grab their location
 firebase.auth().onAuthStateChanged(firebaseUser => {
     if (firebaseUser) {
         console.log(firebaseUser)
         btnLogout.classList.remove('hide');
 
         //Changed to watchPosition for improved Accuracy
+        // This is continually watching the signed in users position and updating
         navigator.geolocation.watchPosition(function (position) {
             console.log(position.coords.longitude, position.coords.latitude);
             longitude = parseFloat(position.coords.longitude);
             latitude = parseFloat(position.coords.latitude);
 
+            // This var holds the information for the current user signed
             var user = firebase.auth().currentUser;
             let name, email, uid;
 
+            // If the user actually exist than do something
             if (user != null) {
                 name = user.displayName;
                 email = user.email;
@@ -75,7 +49,8 @@ firebase.auth().onAuthStateChanged(firebaseUser => {
             console.log(uid);
             console.log(email);
             console.log(name)
-
+            // When they sign in, set their information in the databse to their name,email,uid,
+            // longitude, and latitude. The only thing updated after the first sign in is their location
             database.ref('users/' + uid).set({
                 name: name,
                 email: email,
@@ -83,7 +58,7 @@ firebase.auth().onAuthStateChanged(firebaseUser => {
                 longitude: longitude,
                 latitude: latitude
             });
-
+            // This is what generates the map and it is centered on the users longitude and latitude
             mapboxgl.accessToken = 'pk.eyJ1IjoidGhlbm9vZGxlbW9vc2UiLCJhIjoiY2pvdXM4c3ZrMWZnYTNrbW9ic2hmdjV6ZyJ9.-A735y9fU1TdsJ993uIKLA';
             var map = new mapboxgl.Map({
                 container: 'map', // container id
@@ -91,63 +66,62 @@ firebase.auth().onAuthStateChanged(firebaseUser => {
                 center: [longitude, latitude], // starting position [lng, lat]
                 zoom: 13,
             })
+            // This grabs a snapshot of the current users location and when they move it updates
+            // The firebase information with the new information
             database.ref('users/' + uid).on("value", function (snapshot) {
                 var userLocationLongitude = snapshot.val().longitude;
                 var userLocationLatitude = snapshot.val().latitude;
                 var userName = snapshot.val().name;
                 console.log(userName + " Has Changed Their Location: " + userLocationLongitude + " " + userLocationLatitude);
             });
-
+            // This function is how we grab the information from firebase to mark other users on
+            // the map and make popups with their names
             let updateUserLocation =
+                // This goes into the database and looks at the the users object and grabs a snapshot
                 database.ref('users/').on("value", function (snapshot) {
+                    // Then for each child in snapshot(each user) grab a snapshot of those users
                     snapshot.forEach(function (childsnap) {
                         childsnap.val().name
-                        console.log(childsnap.val().name)
-                        console.log(snapshot)
-                        console.log(childsnap)
-
+                        // For each child in users, create a popup with their name
                         var popup = new mapboxgl.Popup({ closeOnClick: false })
                             .setLngLat([childsnap.val().longitude, childsnap.val().latitude])
                             .setHTML('<p>' + childsnap.val().name + '</p>')
-                            .addTo(map);    
-
-                            var marker = new mapboxgl.Marker()
+                            .addTo(map);
+                        // For each child in users, create a marker at their location
+                        var marker = new mapboxgl.Marker()
                             .setLngLat([childsnap.val().longitude, childsnap.val().latitude])
                             .addTo(map)
-                            
+
                     })
                 })
 
-        //  Users popup box with their user name
-
+            
+            // This creates a popup specifically for the user logged in
             var popup = new mapboxgl.Popup({ closeOnClick: false, closeButton: false })
                 .setLngLat([longitude, latitude])
                 .setHTML("")
                 .setHTML('<p>' + name + '</p>')
                 .addTo(map);
 
-        // zoom in and out buttons
-
+            // zoom in and out buttons
             map.addControl(new mapboxgl.NavigationControl());
 
-        // recenters camera on users icon
-
+            // recenters camera on users icon
             map.on('click', 'points', function (e) {
-                map.flyTo({center: e.features[0].geometry.coordinates, zoom: 13});
+                map.flyTo({ center: e.features[0].geometry.coordinates, zoom: 13 });
             });
 
-        // changes mouse cursor to when its on top a clickable element
-
+            // changes mouse cursor to when its on top a clickable element
             map.on('mouseenter', 'points', function () {
                 map.getCanvas().style.cursor = 'pointer';
             });
-        
-        // chages mouse cursor back to defualt when it leaves clickable elements area
 
+            // chages mouse cursor back to defualt when it leaves clickable elements area
             map.on('mouseleave', 'points', function () {
                 map.getCanvas().style.cursor = '';
-             });
+            });
 
+            // This function runs when the map loads and generates the red marker at the users location
             map.on('load', function () {
                 map.loadImage('https://cdn1.iconfinder.com/data/icons/social-messaging-ui-color/254000/67-512.png', function (error, image) {
                     if (error) throw error;
